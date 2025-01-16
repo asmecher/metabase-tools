669e1013a18843b01ce96978e6f97a514412824f
Alec Smecher <alec@smecher.bc.ca>
2025-01-16 13:17:05 -0800

    Separate native card from parent implementation; extract commonalities

--- src/MetabaseCard.php
+++ src/MetabaseCard.php
@@ -6,46 +6,69 @@ abstract class MetabaseCard {
     /** @var $databaseId The numeric identifier of the Metabase database this card is associated with. */
     protected int $databaseId;
 
+    /**
+     * Construct a metabase card with the specified database ID.
+     */
     public function __construct(int $databaseId) {
 	$this->databaseId = $databaseId;
     }
 
-    abstract function getName() : string;
-    abstract function getDescription() : string;
-    abstract function getDatabaseQuery() : object;
+    abstract public function getName() : string;
+    abstract public function getDescription() : string;
+    abstract public function getDatabaseQuery() : object;
 
+    /**
+     * nullable value must be an integer greater than zero.
+     */
+    public function getCollectionId() : ?int
+    {
+	return null;
+    }
+
+    /**
+     * nullable value must be an integer greater than zero.
+     */
+    public function getCollectionPosition() : ?int
+    {
+	return null;
+    }
+
+    /**
+     * value must be a non-blank string.
+     */
+    public function getDisplay() : string {
+	return 'table';
+    }
+
+    /**
+     * Value must be a map.
+     */
+    public function getVisualizationSettings() : object {
+	return (object) [];
+    }
+
+    /**
+     * nullable value must be an array of valid results column metadata maps.
+     */
+    public function getResultMetadata() : ?array {
+	return null;
+    }
+
+    /**
+     * Get the JSON object representing this Metabase card.
+     */
     function getJson() : object
     {
         $nanoClient = new NanoClient();
         return (object) [
-            'collection_id' => null,
-            'collection_position' => null,
+            'collection_id' => $this->getCollectionId(),
+            'collection_position' => $this->getCollectionPosition(),
             'dataset_query' => $this->getDatabaseQuery(),
             'name' => $this->getName(),
-            'display' => 'table',
+            'display' => $this->getDisplay(),
             'description' => $this->getDescription(),
-            'parameters' => [
-                (Object) [
-                    'default' => '2023-01-01',
-                    'id' => $nanoClient->generateId(21), // This is a unique ID for import/export purposes
-                    'name' => 'Start Date',
-                    'required' => true,
-                    'slug' => 'start_date',
-                    'target' => ['variable', ['template-tag', 'start_date']],
-                    'type' => 'date/single',
-                ],
-                (Object) [
-                    'default' => '2023-12-31',
-                    'id' => $nanoClient->generateId(21), // This is a unique ID for import/export purposes
-                    'name' => 'End Date',
-                    'required' => true,
-                    'slug' => 'end_date',
-                    'target' => ['variable', ['template-tag', 'end_date']],
-                    'type' => 'date/single',
-                ],
-            ],
-            'result_metadata' => null,
-            'visualization_settings' => (Object) [],
+            'result_metadata' => $this->getResultMetadata(),
+            'visualization_settings' => $this->getVisualizationSettings(),
         ];
     }
 }
---
+++ src/NativeCard.php
@@ -0,0 +1,48 @@
+<?php
+
+use Hidehalo\Nanoid\Client as NanoClient;
+
+require_once('src/MetabaseCard.php');
+
+abstract class NativeCard extends MetabaseCard {
+    abstract public function getName() : string;
+    abstract public function getDescription() : string;
+    abstract public function getQuery() : string;
+
+    /**
+     * nullable sequence of parameter must be a map with :id and :type keys.
+     */
+    public function getParameters() : array
+    {
+	return [];
+    }
+
+    /**
+     * (undocumented Metabase API component?)
+     */
+    public function getTemplateTags() : array
+    {
+	return [];
+    }
+
+    public function getDatabaseQuery() : object
+    {
+        $nanoClient = new NanoClient();
+        return (object) [
+            'database' => $this->databaseId,
+            'native' => [
+                'query' => $this->getQuery(),
+		'template-tags' => $this->getTemplateTags(),
+            ],
+            'type' => 'native',
+        ];
+    }
+
+    public function getJson() : object
+    {
+	$json = parent::getJson();
+	$json->parameters = $this->getParameters();
+	return $json;
+    }
+}
+
