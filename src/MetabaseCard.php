5d8c53265ebb5beb8ecb988daaac86cd8209c52a
Alec Smecher <alec@smecher.bc.ca>
2025-01-16 11:48:13 -0800

    Split query fetch and JSON fetch into separate functions

--- src/MetabaseCard.php
+++ src/MetabaseCard.php
@@ -1,5 +1,7 @@
 <?php
 
+use Hidehalo\Nanoid\Client as NanoClient;
+
 abstract class MetabaseCard {
     /** @var $databaseId The numeric identifier of the Metabase database this card is associated with. */
     protected int $databaseId;
@@ -10,5 +12,40 @@ abstract class MetabaseCard {
 
     abstract function getName() : string;
     abstract function getDescription() : string;
-    abstract function getJson() : array;
+    abstract function getDatabaseQuery() : object;
+
+    function getJson() : object
+    {
+        $nanoClient = new NanoClient();
+        return (object) [
+            'collection_id' => null,
+            'collection_position' => null,
+            'dataset_query' => $this->getDatabaseQuery(),
+            'name' => $this->getName(),
+            'display' => 'table',
+            'description' => $this->getDescription(),
+            'parameters' => [
+                (Object) [
+                    'default' => '2023-01-01',
+                    'id' => $nanoClient->generateId(21), // This is a unique ID for import/export purposes
+                    'name' => 'Start Date',
+                    'required' => true,
+                    'slug' => 'start_date',
+                    'target' => ['variable', ['template-tag', 'start_date']],
+                    'type' => 'date/single',
+                ],
+                (Object) [
+                    'default' => '2023-12-31',
+                    'id' => $nanoClient->generateId(21), // This is a unique ID for import/export purposes
+                    'name' => 'End Date',
+                    'required' => true,
+                    'slug' => 'end_date',
+                    'target' => ['variable', ['template-tag', 'end_date']],
+                    'type' => 'date/single',
+                ],
+            ],
+            'result_metadata' => null,
+            'visualization_settings' => (Object) [],
+        ];
+    }
 }
